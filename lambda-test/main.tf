dc57debeaf794bf6d4fbf9ef06094debcd70b1cc
Tom Flynn <thomasjflynn3@gmail.com>
2022-08-16 16:09:12 -0700

    Update main.tf

--- lambda-test/main.tf
+++ lambda-test/main.tf
@@ -56,7 +56,7 @@ resource "aws_lambda_function" "test_lambda" {
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  source_code_hash = filebase64sha256(archive_file.source_code.output_path)
+  source_code_hash = filebase64sha256(data.archive_file.source_code.output_path)
 
   runtime = "python3.9"
 
