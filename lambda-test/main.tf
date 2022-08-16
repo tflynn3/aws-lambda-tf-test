439c5444bf6f414e581721ce73bdfface480e1c5
Tom Flynn <thomasjflynn3@gmail.com>
2022-08-16 16:19:22 -0700

    Update main.tf

--- lambda-test/main.tf
+++ lambda-test/main.tf
@@ -49,9 +49,9 @@ resource "aws_lambda_function" "test_lambda" {
   # If the file is not in the current working directory you will need to include a 
   # path.module in the filename.
   filename      = "lambda_function_payload.zip"
-  function_name = "lambda_function_name"
+  function_name = "hello_world"
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "lambda_handler"
+  handler       = "hello_world.lambda_handler"
 
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
