28c7e74349e93aed937c05e718ffb388dd5c2aa4
Tom Flynn <thomasjflynn3@gmail.com>
2022-08-16 15:36:34 -0700

    Update main.tf

--- lambda-test/main.tf
+++ lambda-test/main.tf
@@ -6,6 +6,23 @@ terraform {
   }
 }
 
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      Version = "~>3.27"
+    }
+  }
+
+  required_version = ">=0.14.9"
+
+}
+
+provider "aws" {
+  version = "~>3.0"
+  region  = "east-us-1"
+}
+
 ### Cloud Function
 data "archive_file" "cf_code_ambr250" {
   type        = "zip"
